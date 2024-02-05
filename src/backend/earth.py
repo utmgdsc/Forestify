4a922062fc672e210ff694cc04f2a40a23ede1ff
Asad Khan <asadkhan@Asads-MacBook-Pro.local>
2024-02-05 11:59:30 -0500

    Add Canada Forest Information

--- src/backend/earth.py
+++ src/backend/earth.py
@@ -3,20 +3,31 @@ from google.oauth2 import service_account
 from pprint import pprint
 import json
 import urllib
+import ee
 
-credentials = service_account.Credentials.from_service_account_file('private-key.json')
-scoped_credentials = credentials.with_scopes(
-    ['https://www.googleapis.com/auth/cloud-platform'])
+ee.Authenticate()
+ee.Initialize(project='forestify-project')
 
-authed_session = AuthorizedSession(scoped_credentials)
 
-project = 'projects/earthengine-public'
 asset_id = 'UMD/hansen/global_forest_change_2022_v1_10'
 
-name = '{}/assets/{}'.format(project, asset_id)
+lc = ee.Image(asset_id)
+print(lc.getInfo())
 
-url = 'https://earthengine.googleapis.com/v1alpha/{}'.format(name)
+# Get the loss info for Canada
 
-response = authed_session.get(url)
+loss = lc.select('loss')
+loss_canada = loss.reduceRegion(reducer=ee.Reducer.sum(), geometry=ee.Geometry.Rectangle(-140, 50, -52, 60), scale=30, maxPixels=1e9, bestEffort=True)
+print(loss_canada.getInfo())
 
-pprint(response.json()['bands'][1])
+# Get the gain info for Canada
+
+gain = lc.select('gain')
+gain_canada = gain.reduceRegion(reducer=ee.Reducer.sum(), geometry=ee.Geometry.Rectangle(-140, 50, -52, 60), scale=30, maxPixels=1e9, bestEffort=True)
+print(gain_canada.getInfo())
+
+# Get the tree cover info for Canada
+
+tree_cover = lc.select('treecover2000')
+tree_cover_canada = tree_cover.reduceRegion(reducer=ee.Reducer.mean(), geometry=ee.Geometry.Rectangle(-140, 50, -52, 60), scale=30, maxPixels=1e9, bestEffort=True)
+print(tree_cover_canada.getInfo())
