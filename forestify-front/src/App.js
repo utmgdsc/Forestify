fcb5e5d2bf0de6264472855f42425e8fd5e0f100
Azfar Choudhry <azfar.choudhry@mail.utoronto.ca>
2024-02-10 15:31:56 -0500

    made signup/login mockup

---
+++ forestify-front/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { LoginSignup } from './components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div >
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
---
+++ forestify-front/src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import appLogo from '../Assets/logo.png'
+
+export const LoginSignup = () => {
+
+  const [action, setAction] = React.useState('Sign Up')
+
+  return (
+    
+    <div className='page'>
+
+      <div className='logo'>
+        <img src={appLogo} alt='logo' />
+          <div>Forestify.ai</div>
+      </div> 
+
+      <div className='container'>
+        <div className='header'>
+          <div className='text'>{action}</div>
+          <div className='underline'></div>
+        </div>
+        
+        <div className='inputs'>
+        {action==="Login"?<div></div>:<div className='input'>
+            <img src={user_icon} alt='name' />
+            <input type='text' placeholder='Name' />
+          </div>}
+          <div className='input'>
+            <img src={email_icon} alt='email' />
+            <input type='email' placeholder='Email' />
+          </div>
+          <div className='input'>
+            <img src={password_icon} alt='password' />
+            <input type='password' placeholder='Password' />
+          </div>
+        </div>
+
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+
+
+        <div className='submit-container'>
+          <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+          <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}} >Log In</div>
+
+        </div>
+      </div>
+    </div>
+    
+  )
+}
