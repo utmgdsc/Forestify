3f215356dce81286b704bfad3b1bee96581875fb
Asad Khan <asadkhan@Asads-MacBook-Pro.local>
2024-02-12 13:50:12 -0500

    Forest Map Visualization

--- src/backend/earth.py
+++ src/backend/earth.py
@@ -4,28 +4,67 @@ from pprint import pprint
 import json
 import urllib
 import ee
+import folium
+
 
 ee.Authenticate()
 ee.Initialize(project='forestify-project')
 
 
-asset_id = 'UMD/hansen/global_forest_change_2022_v1_10'
+aoi = ee.Geometry.Rectangle([-106.3468, 56.1304, -98.3468, 60.1304])
 
-lc = ee.Image(asset_id)
-print(lc.getInfo())
-print('\n')
 
-countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017')
-canada = countries.filter(ee.Filter.eq('country_na', 'Canada'))
+sentinel_1 = ee.ImageCollection('COPERNICUS/S1_GRD')\
+    .filterBounds(aoi)\
+    .filterDate('2023-01-01', '2023-12-31')\
+    .filter(ee.Filter.eq('instrumentMode', 'IW'))\
+    .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))\
+    .select('VV')
 
-lossImage = lc.select('loss')
+sentinel_2 = ee.ImageCollection('COPERNICUS/S2')\
+    .filterBounds(aoi)\
+    .filterDate('2023-01-01', '2023-12-31')\
+    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))\
+    .select(['B4', 'B3', 'B2', 'B8'])
 
-stats = lossImage.reduceRegion(
-    reducer=ee.Reducer.sum(),
-    geometry=canada.geometry(),
-    scale=30,
-    maxPixels=1e9,
-    bestEffort=True
-    )
 
-print(stats.getInfo())
+vis_params_s1 = {
+    'min': -25,
+    'max': 0,
+    'palette': ['blue', 'green', 'red']
+}
+
+
+vis_params_s2 = {
+    'min': 0,
+    'max': 3000,
+    'bands': ['B4', 'B3', 'B2']  # True color composite
+}
+
+map_id_s1 = ee.Image(sentinel_1.mean()).getMapId(vis_params_s1)
+map_id_s2 = ee.Image(sentinel_2.mean()).getMapId(vis_params_s2)
+
+
+html_template = f"""
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sentinel Map Visualization</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <style> #map {{ width: 100%; height: 600px; }} </style>
+</head>
+<body>
+    <div id="map"></div>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script>
+        var map = L.map('map').setView([58.1304, -102.3468], 5);
+        L.tileLayer('https://earthengine.googleapis.com/v1alpha/{map_id_s1['mapid']}/tiles/{{z}}/{{x}}/{{y}}?access_token={map_id_s1['token']}').addTo(map);
+        L.tileLayer('https://earthengine.googleapis.com/v1alpha/{map_id_s2['mapid']}/tiles/{{z}}/{{x}}/{{y}}?access_token={map_id_s2['token']}').addTo(map);
+    </script>
+</body>
+</html>
+"""
+
+
+with open('map_visualization.html', 'w') as file:
+    file.write(html_template)
