61bd0a072d61b89efc2717da3b88e4ecbf646b89
Azfar Choudhry <azfar.choudhry@mail.utoronto.ca>
2024-03-04 12:06:52 -0500

    stopped csrf issue

--- forestify_back/forestify/forestify/settings.py
+++ forestify_back/forestify/forestify/settings.py
@@ -134,10 +134,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.SessionAuthentication',
+    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     ],
 }
