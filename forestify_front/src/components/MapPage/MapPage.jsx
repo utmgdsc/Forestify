f350911ef79b957dc4ca549c699908635f315d98
Azfar Choudhry <azfar.choudhry@mail.utoronto.ca>
2024-04-01 01:33:51 -0400

    fixes

--- forestify_front/src/components/MapPage/MapPage.jsx
+++ forestify_front/src/components/MapPage/MapPage.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './MapPage.css';
-import { Link } from "react-router-dom";
-import { GrMapLocation } from 'react-icons/gr';
-import { SidebarData } from './SidebarData';
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { OpenStreetMapProvider } from "react-leaflet-geosearch";
 import MapSearch from './MapSearch';
+// import { map } from 'leaflet';
+
+// Used to recenter the map to new coordinates
+const MapRecenter= ({ lat, lng, zoomLevel }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    // Fly to that coordinates and set new zoom level
+    map.flyTo([lat, lng], zoomLevel );
+  }, [lat, lng]);
+  return null;
+
+};
 
 export const MapPage = () => {
   //const [sidebar, setSidebar] = useState(false);
   const [coordinates, setCoordinates] = useState({ location: '', startDate: '', endDate: '', latitude: '', longitude: '', analysis: '' }); // State to hold coordinates, analysis option, and dates
-
+  const [mapLink, setMapLink] = useState("https://earthengine.googleapis.com/v1/projects/forestify-project/maps/4b724472b02ca7bc0eac108ad62aab6c-4a4abeb009b2b548b06b44fe70786f54/tiles/{z}/{x}/{y}");
+  const [lat, setLat] = useState(43.6532);
+  const [lon, setLon] = useState(-79.3832);
   //const showSidebar = () => setSidebar(!sidebar);
   const prov = OpenStreetMapProvider();
+
+  // let lat = 43.6532;
+  // let lon = -79.3832;
+  // let mapLink = "https://earthengine.googleapis.com/v1/projects/forestify-project/maps/4b724472b02ca7bc0eac108ad62aab6c-4a4abeb009b2b548b06b44fe70786f54/tiles/{z}/{x}/{y}"
+
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -29,15 +47,22 @@ export const MapPage = () => {
       const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${coordinates.location}`);
       const data = await response.json();
       if (data && data.length > 0) {
-        const { lat, lon } = data[0];
-        const jsonData = {
-          latitude: lat,
-          longitude: lon,
-          analysis: coordinates.analysis,
-          startDate: coordinates.startDate,
-          endDate: coordinates.endDate
-        };
-        console.log("Raw JSON:", jsonData);
+
+        setLat(data[0]['lat']);
+        setLon(data[0]['lon']);
+        
+        try{
+        const mapResponse = await fetch(`http://127.0.0.1:8000/getMap?latitude=${lat}&longitude=${lon}&analysis=${coordinates.analysis}&startDate=${coordinates.startDate}&endDate=${coordinates.endDate}`);
+        const mapLinkResponse = await mapResponse.json();
+        
+        setMapLink(mapLinkResponse['MapLink']);
+
+        // console.log(`http://127.0.0.1:8000/getMap?latitude=${lat}&longitude=${lon}&analysis=${coordinates.analysis}&startDate=${coordinates.startDate}&endDate=${coordinates.endDate}`)
+        console.log(mapLink);
+
+        } catch (error) {
+          console.error("Error fetching map:", error);
+        }
       } else {
         console.log("Location not found");
       }
@@ -45,34 +70,21 @@ export const MapPage = () => {
       console.error("Error fetching location:", error);
     }
   }
-  
+
+
+
   return (
     <>
-      {/* <div className="navbar"> 
-        <Link to="#" className='menu-bars'>
-          <GrMapLocation onClick={showSidebar}/>
-        </Link>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className='nav-menu-items' onClick={showSidebar}>
-            {SidebarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
-      </div> */}
 
       <div className="map-container-wrapper">
-        <MapContainer center={[51.505, -0.091]} zoom={13}>
+        <MapContainer center={[lat,lon]} zoom={10}>
+          <MapRecenter lat={lat} lng={lon} zoomLevel={10} />
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          <TileLayer
+            url={mapLink}
           />
           <MapSearch
             provider={prov}
